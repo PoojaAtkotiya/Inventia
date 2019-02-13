cb1c2f49b0534bd2d001e449eec4416120aeb8d6
Pooja Atkotiya <35293527+PoojaAtkotiya@users.noreply.github.com>
2019-02-13 19:51:50 +0530

    Updated for save data while null listdataarray

--- JS/Common.js
+++ JS/Common.js
@@ -127,7 +127,7 @@ function onloadConstantsSuccess(sender, args) {
         setImageSignature();
     }
 
-    GetFormBusinessLogic(activeSectionName,department);
+    GetFormBusinessLogic(activeSectionName, department);
     //setCustomApprovers();
 }
 
@@ -1260,119 +1260,127 @@ function SaveData(listname, listDataArray, sectionName, ele) {
             headers = { "Accept": "application/json;odata=verbose", "Content-Type": "application/json;odata=verbose", "X-RequestDigest": $("#__REQUESTDIGEST").val() };
         }
 
-        AjaxCall({
-            url: url,
-            postData: JSON.stringify(listDataArray),
-            httpmethod: 'POST',
-            calldatatype: 'JSON',
-            headers: headers,
-            contentType: 'application/json; charset=utf-8',
-            async: false,
-            sucesscallbackfunction: function (data) {
-                var itemID = listItemId;
-                if (!IsNullOrUndefined(data) && !IsNullOrUndefined(data.d)) {
-                    itemID = data.d.ID;
+        if (!IsNullOrUndefined(listDataArray) && listDataArray.length > 0) {
+            AjaxCall({
+                url: url,
+                postData: JSON.stringify(listDataArray),
+                httpmethod: 'POST',
+                calldatatype: 'JSON',
+                headers: headers,
+                contentType: 'application/json; charset=utf-8',
+                async: false,
+                sucesscallbackfunction: function (data) {
+                    OnSuccessMainListSave(data, sectionName, buttonCaption);
+                },
+                error: function (data) {
+                    console.log(data);
+                    HideWaitDialog();
                 }
-                ////AddAttachments(itemID);
-                //  AddAllAttachments(listname, itemID);
-                var web, clientContext;
-                SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
-                    clientContext = new SP.ClientContext.get_current();
-                    web = clientContext.get_web();
-                    oList = web.get_lists().getByTitle(listname);
-                    var oListItem = oList.getItemById(itemID);
-                    clientContext.load(oListItem, 'FormLevel', 'RaisedBy');
-                    clientContext.load(web);
-                    clientContext.executeQueryAsync(function () {
-                        CommonBusinessLogic(sectionName, itemID, listDataArray);
-                        SaveLocalApprovalMatrix(sectionName, itemID, listname, isNewItem, oListItem, ListNames.APPROVALMATRIXLIST);
-                        SaveActivityLog(sectionName, itemID, ListNames.ACTIVITYLOGLIST, listDataArray, isNewItem, buttonCaption);
-                        if (!isNaN(itemID)) {
-                            debugger
-                            // SaveTranListData(itemID);
-                            SaveAllTrans(itemID);
-                        }
-                        // else {
-                        //     SaveTranListData(itemID);
-                        // }
-                        HideWaitDialog();
-                        if (IsNullOrUndefined(data)) {
-                            data = {};
-                            data = {
-                                ItemID: itemID,
-                                IsSucceed: true,
-                                Messages: "Data saved successfully"
-                            }
-                        }
-                        else {
-                            data.ItemID = itemID;
-                            data.IsSucceed = true;
-                            data.Messages = "Data saved successfully";
-                        }
-                        if (buttonCaption.toLowerCase() == "save as draft" || buttonCaption.toLowerCase() == "resume") {
-                            OnSuccessNoRedirect(data);
-                        }
-                        else if (buttonCaption.toLowerCase() == "complete" && !isPageRedirect) {
-                            OnSuccessConfirmSubmitNoRedirect(data);
-                        }
-                        else {
-                            OnSuccess(data);
-                        }
-                    }, function (sender, args) {
-                        HideWaitDialog();
-                        console.log('request failed ' + args.get_message() + '\n' + args.get_stackTrace());
-                    });
-                });
-            },
-            error: function (data) {
-                console.log(data);
-                HideWaitDialog();
-            }
-        });
+            });
+        }
+        else {
+            OnSuccessMainListSave(null, sectionName, buttonCaption);
+        }
+
     }
+}
+
+function OnSuccessMainListSave(data, sectionName, buttonCaption) {
+    var itemID = listItemId;
+    if (!IsNullOrUndefined(data) && !IsNullOrUndefined(data.d)) {
+        itemID = data.d.ID;
+    }
+    var web, clientContext;
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+        clientContext = new SP.ClientContext.get_current();
+        web = clientContext.get_web();
+        oList = web.get_lists().getByTitle(ListNames.MAINLIST);
+        var oListItem = oList.getItemById(itemID);
+        clientContext.load(oListItem, 'FormLevel', 'RaisedBy');
+        clientContext.load(web);
+        clientContext.executeQueryAsync(function () {
+            CommonBusinessLogic(sectionName, itemID, listDataArray);
+            SaveLocalApprovalMatrix(sectionName, itemID, listname, isNewItem, oListItem, ListNames.APPROVALMATRIXLIST);
+            SaveActivityLog(sectionName, itemID, ListNames.ACTIVITYLOGLIST, listDataArray, isNewItem, buttonCaption);
+            if (!isNaN(itemID)) {
+                debugger
+                // SaveTranListData(itemID);
+                SaveAllTrans(itemID);
+            }
+            // else {
+            //     SaveTranListData(itemID);
+            // }
+            HideWaitDialog();
+            if (IsNullOrUndefined(data)) {
+                data = {};
+                data = {
+                    ItemID: itemID,
+                    IsSucceed: true,
+                    Messages: "Data saved successfully"
+                }
+            }
+            else {
+                data.ItemID = itemID;
+                data.IsSucceed = true;
+                data.Messages = "Data saved successfully";
+            }
+            if (buttonCaption.toLowerCase() == "save as draft" || buttonCaption.toLowerCase() == "resume") {
+                OnSuccessNoRedirect(data);
+            }
+            else if (buttonCaption.toLowerCase() == "complete" && !isPageRedirect) {
+                OnSuccessConfirmSubmitNoRedirect(data);
+            }
+            else {
+                OnSuccess(data);
+            }
+        }, function (sender, args) {
+            HideWaitDialog();
+            console.log('request failed ' + args.get_message() + '\n' + args.get_stackTrace());
+        });
+    });
 }
 
 function CommonBusinessLogic(sectionName, itemID, listDataArray) {
     var actionStatus = $("#ActionStatus").val();
-    
+
     var keys = Object.keys(ButtonActionStatus).filter(k => ButtonActionStatus[k] == actionStatus);
     var actionPerformed = keys.toString();
     SaveImageSignaturePath(sectionName, itemID);
-    SaveActions(sectionName,itemID,actionPerformed);
+    SaveActions(sectionName, itemID, actionPerformed);
     if (sectionName == SectionNames.INITIATORSECTION && actionPerformed == "NextApproval") {
-        SaveCapitalAssetRequisitionNumber(itemID, listDataArray,actionPerformed);
+        SaveCapitalAssetRequisitionNumber(itemID, listDataArray, actionPerformed);
     }
 
 }
-function SaveActions(sectionName,itemID,actionPerformed) {
+function SaveActions(sectionName, itemID, actionPerformed) {
     var formFieldValues = [];
     var todayDate = new Date();
     switch (sectionName) {
         case SectionNames.INITIATORSECTION:
-        if(actionPerformed=="NextApproval"){
-        formFieldValues['InitiatorAction'] = currentUser.Title + '-' + todayDate + '-' + "Submit";
-        }
-        else if(actionPerformed=="SaveAsDraft"){
-            formFieldValues['InitiatorAction'] = currentUser.Title + '-' + todayDate + '-' + "SaveAsDraft";
-        }
+            if (actionPerformed == "NextApproval") {
+                formFieldValues['InitiatorAction'] = currentUser.Title + '-' + todayDate + '-' + "Submit";
+            }
+            else if (actionPerformed == "SaveAsDraft") {
+                formFieldValues['InitiatorAction'] = currentUser.Title + '-' + todayDate + '-' + "SaveAsDraft";
+            }
             break;
         case SectionNames.HODSECTION:
-          // formFieldValues['HODSignature'] = data.d.results[0].FileRef;
+            // formFieldValues['HODSignature'] = data.d.results[0].FileRef;
             break;
         case SectionNames.PURCHASESECTION:
-          //  formFieldValues['PurchaseSignature'] = data.d.results[0].FileRef;
+            //  formFieldValues['PurchaseSignature'] = data.d.results[0].FileRef;
             break;
         case SectionNames.FUNCTIONHEADSECTION:
-          //  formFieldValues['FunctionHeadSignature'] = data.d.results[0].FileRef;
+            //  formFieldValues['FunctionHeadSignature'] = data.d.results[0].FileRef;
             break;
         case SectionNames.MANAGEMENTSECTION:
-           // formFieldValues['ManagementSignature'] = data.d.results[0].FileRef;
+            // formFieldValues['ManagementSignature'] = data.d.results[0].FileRef;
             break;
     }
-   
+
     SaveFormFields(formFieldValues, itemID);
 }
-function SaveCapitalAssetRequisitionNumber(itemID, listDataArray,actionPerformed) {
+function SaveCapitalAssetRequisitionNumber(itemID, listDataArray, actionPerformed) {
     var formFieldValues = [];
     var todayDate = new Date();
     formFieldValues['CapitalAssetRequisitionNumber'] = listDataArray.CostCenter + '/' + todayDate.getFullYear() + ("0" + (this.getMonth() + 1)).slice(-2) + '/' + itemID;
