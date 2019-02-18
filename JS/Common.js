8e0309c6e9b8913449897744531c4f61b318ae5a
Pooja Atkotiya <35293527+PoojaAtkotiya@users.noreply.github.com>
2019-02-18 19:23:11 +0530

    update for complete action status on WF

--- JS/Common.js
+++ JS/Common.js
@@ -1575,7 +1575,7 @@ function SaveImageSignaturePath(sectionName, itemID) {
                         formFieldValues['PurchaseSignature'] = data.d.results[0].FileRef;
                         break;
                     case SectionNames.FUNCTIONHEADSECTION:
-                        //formFieldValues['FunctionHeadSignature'] = data.d.results[0].FileRef;
+                        formFieldValues['FunctionHeadSignature'] = data.d.results[0].FileRef;
                         break;
                     case SectionNames.MANAGEMENTSECTION:
                         formFieldValues['ManagementSignature'] = data.d.results[0].FileRef;
