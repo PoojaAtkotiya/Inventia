ad6cbdd2bcbc824089243dd366a03eefe2a5891d
Pooja Atkotiya <35293527+PoojaAtkotiya@users.noreply.github.com>
2019-03-12 19:21:58 +0530

    updated

--- JS/AppConstants.js
+++ JS/AppConstants.js
@@ -9,8 +9,8 @@ const CommonConstant = {
 
     SAVEACTIVITYLOGFLOW: "https://prod-10.centralindia.logic.azure.com:443/workflows/d34b6d4c22b0448d852ac684b00f6e91/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DuR9T49zRIZda3lRSCicc7UVlojrPm48VoznpHmXXKI",
     SAVEAPPSTATUSFLOW: "https://prod-14.centralindia.logic.azure.com:443/workflows/f537cbbc0737453597e9acaaee919239/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6GnqYyXD0KZnUbZJP7wjXFOH8gKOhWR8VGN8YCfEK4w",
+
     /*Dev */
-    /**/
     SETITEMPERMISSION: "https://prod-05.centralindia.logic.azure.com:443/workflows/068a2cd297de410ab19fc808be3a2735/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=eSrKcDBAxAwGqTouTX6xZ8sUpoQIl--H02rmq8EW3c8",
     SAVEEMAILINLIST: "https://prod-03.centralindia.logic.azure.com:443/workflows/2aebe35deb8b46768a76916d9aec7af9/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r1Z9IKQ91LLVBHA08SD76oAQq67XvWw_ecOCl_V8Mj4",
     MAINLISTEDITURL: "/sites/dev/Pages/InventiaNew.aspx?ID="
@@ -19,7 +19,7 @@ const CommonConstant = {
     SETITEMPERMISSION: "https://prod-09.centralindia.logic.azure.com:443/workflows/af272fa2e21b4ccab37d736a790f7424/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Rzv-URVzOF6Jxclr8_I4f4rcbtCUUiB8OzBPRRviJZU",
     SAVEEMAILINLIST: "https://prod-28.centralindia.logic.azure.com:443/workflows/39aadd88abae45ce92cc56e5d60c16c6/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=gZi0rLYpjmoGdTEr_Zg6FZvx12v_NKXyyGEhnpVzDQ8",
      MAINLISTEDITURL: "/sites/QACapex/Pages/CapitalAssetRequisition.aspx?ID=",
-*/
+    */
 
 }
 Object.freeze(CommonConstant);
