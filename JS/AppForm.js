3108b7d37dddd86a5604c863f3fcd1e484c6b6bf
Priya Rane <ranepriy@SYNOVERGETECH.COM>
2019-03-27 10:27:32 +0530

    changes

--- JS/AppForm.js
+++ JS/AppForm.js
@@ -1648,6 +1648,30 @@ function ValidateSize(file) {
     return isValid;
 }
 
+var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png", ".doc"];    
+function ValidateSingleInput(oInput) {
+    if (oInput.type == "file") {
+        var sFileName = oInput.value;
+         if (sFileName.length > 0) {
+            var blnValid = false;
+            for (var j = 0; j < _validFileExtensions.length; j++) {
+                var sCurExtension = _validFileExtensions[j];
+                if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                    blnValid = true;
+                    break;
+                }
+            }
+             
+            if (!blnValid) {
+                AlertModal('Error', "Invalid extension of file");
+                oInput.value = "";
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 function ReplaceNumberWithCommas(yourNumber) {
     if (yourNumber != null) {
         var n = yourNumber.toString().split(".");
